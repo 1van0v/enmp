3404c0ddb621364685cb142696820e005d61c9d7
Sashka <ivanov5oleksandr@gmail.com>
2020-08-03 22:39:54 +0300

    task 1

---
+++ hw_1/task1.js
@@ -0,0 +1,7 @@
+const readline = require('readline');
+
+const rl = readline.createInterface(process.stdin);
+
+rl.on('line', function (line) {
+  console.log(line.split('').reverse().join(''), '\n');
+})
