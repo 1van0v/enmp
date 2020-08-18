b563b7d405492197ab635e95d2a987b51d8143a6
Sashka <ivanov5oleksandr@gmail.com>
2020-08-18 10:25:33 +0300

    add user

--- hw_2/index.js
+++ hw_2/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { v4 as uuid } from 'uuid';
 
 import logger from './utils/logger';
 import { users } from './users';
@@ -10,17 +11,37 @@ const app = express();
 app.use(express.json());
 app.use(logger);
 
-app.get('/api/users', (req, res) => {
+app.get('/users', (req, res) => {
     return res.json({ users });
 });
 
-app.get('/api/users/:id', (req, res) => {
+app.get('/users/:id', (req, res) => {
     const { id } = req.params;
     const user = users.find((i) => i.id === id);
 
     return user
         ? res.json(user)
         : res.status(404).json({ error: `Cannot find a user with ID ${id}` });
+});
+
+app.post('/users', (req, res) => {
+    console.log('received a new user');
+    const { login, password, age } = req.body;
+
+    if (!(login && password && age)) {
+        return res.status(400).json({ error: 'Required fields are missed' });
+    }
+
+    const newUser = {
+        id: uuid(),
+        login,
+        password,
+        age,
+        isDeleted: false
+    };
+
+    users.push(newUser);
+    res.json(newUser);
 });
 
 app.listen(port, () => {
