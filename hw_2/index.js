31c6f35d98034aa7c729ab4765321e2cf37557c6
Sashka <ivanov5oleksandr@gmail.com>
2020-08-19 01:47:23 +0300

    add joi validation

--- hw_2/index.js
+++ hw_2/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { v4 as uuid } from 'uuid';
+import Joi from '@hapi/joi';
+import { createValidator } from 'express-joi-validation';
 
 import logger from './utils/logger';
 import { getAutoSuggestUsers } from './utils/getAutoSuggestUsers';
@@ -8,6 +10,20 @@ import { users } from './users';
 const port = 3000;
 
 const app = express();
+const validator = createValidator({ passError: true });
+
+const addUserSchema = Joi.object({
+    login: Joi.string().min(1).required(),
+    password: Joi.string().alphanum().min(1).required(),
+    age: Joi.number().integer().min(4).max(130).required()
+});
+
+const updateUserSchema = Joi.object({
+    login: Joi.string().min(1),
+    password: Joi.string().alphanum().min(1),
+    age: Joi.number().integer().min(4).max(130),
+    isDeleted: Joi.boolean()
+});
 
 app.use(express.json());
 app.use(logger);
@@ -33,12 +49,8 @@ app.get('/users/:id', (req, res) => {
         : res.status(404).json({ error: `Cannot find a user with ID ${id}` });
 });
 
-app.post('/users', (req, res) => {
+app.post('/users', validator.body(addUserSchema), (req, res) => {
     const { login, password, age } = req.body;
-
-    if (!(login && password && age)) {
-        return res.status(400).json({ error: 'Required fields are missed' });
-    }
 
     const newUser = {
         id: uuid(),
@@ -52,14 +64,18 @@ app.post('/users', (req, res) => {
     res.json(newUser);
 });
 
-app.put('/users/:id', (req, res) => {
+app.put('/users/:id', validator.body(updateUserSchema), (req, res) => {
+    if (!Object.keys(req.body).length) {
+        return res.status(400).json({ error: 'specify update values' });
+    }
+
     const userToUpdate = users.find((i) => i.id === req.params.id);
 
     if (!userToUpdate) {
         return res.status(400).json({ error: 'Cannot find such user' });
     }
 
-    ['login', 'password', 'age'].forEach((key) => {
+    ['login', 'password', 'age', 'isDeleted'].forEach((key) => {
         const update = req.body[key];
         if (update) {
             userToUpdate[key] = update;
@@ -79,6 +95,16 @@ app.delete('/users/:id', (req, res) => {
     userToUpdate.isDeleted = true;
 
     res.status(200).json(userToUpdate);
+});
+
+app.use((err, req, res, next) => {
+    if (err && err.error && err.error.isJoi) {
+        return res.status(400).json({
+            message: err.error.toString()
+        });
+    }
+
+    return next(err);
 });
 
 app.listen(port, () => {
