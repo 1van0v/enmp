0a277e6356d8966ba10b5f8fb7b3207026bd4b88
Sashka <ivanov5oleksandr@gmail.com>
2020-09-27 23:04:35 +0300

    update data access layer #9

--- hw_2/models/groups.js
+++ hw_2/models/groups.js
@@ -1,21 +1,49 @@
 import { Sequelize, DataTypes } from 'sequelize';
 
-export const GroupsModel = sequelize => sequelize.define(
-    'Groups',
-    {
-        id: {
-            type: DataTypes.UUID,
-            primaryKey: true,
-            defaultValue: Sequelize.UUIDV4
+export function GroupsModel(sequelize) {
+    const model = sequelize.define(
+        'Groups',
+        {
+            id: {
+                type: DataTypes.UUID,
+                primaryKey: true,
+                defaultValue: Sequelize.UUIDV4
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            permissions: {
+                type: DataTypes.ARRAY(DataTypes.ENUM('READ', 'WRITE', 'DELETE', 'SHARE', 'UPLOAD_FILES')),
+                allowNull: false
+            }
         },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        permissions: {
-            type: DataTypes.ARRAY(DataTypes.ENUM('READ', 'WRITE', 'DELETE', 'SHARE', 'UPLOAD_FILES')),
-            allowNull: false
+        { tableName: 'groups', timestamps: false }
+    );
+
+    model.getGroup = id => model.findByPk(id);
+    model.getGroups = () => model.findAll();
+    model.updateGroup = (id, update) => model.update(update, { where: { id } });
+    model.deleteGroup = id => model.destroy({ where: { id } });
+
+    model.addUsersToGroup = async (groupId, userIds) => {
+        const transaction = await sequelize.transaction();
+
+        try {
+            const updates = userIds.map(userId => sequelize.model('UserGroup').create(
+                {
+                    user_id: userId,
+                    group_id: groupId
+                },
+                { transaction }
+            ));
+
+            await Promise.all(updates);
+            await transaction.commit();
+        } catch (e) {
+            await transaction.rollback();
         }
-    },
-    { tableName: 'groups', timestamps: false }
-);
+    };
+
+    return model;
+}
